90751516cf538c6aabb37ef4ca61322277e527f1
MummidiLakshman <96734843+MummidiLakshman@users.noreply.github.com>
2022-09-09 11:49:38 +0530

    import java.util.Scanner; class avg {     public  static void main(String args[])     {         char a;         Scanner sc=new Scanner(System.in);         a=sc.next().charAt(0);         System.out.println((int)a);      sc.close();     } }

---
+++ Find_ASCII_Value_of_a_Character.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class avg
+{
+    public  static void main(String args[])
+    {
+        char a;
+        Scanner sc=new Scanner(System.in);
+        a=sc.next().charAt(0);
+        System.out.println((int)a);
+     sc.close();
+    }
+}
