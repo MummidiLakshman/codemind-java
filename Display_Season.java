df4cffab4a79d0e64882fbbd7c20203a7ebc04a7
MummidiLakshman <96734843+MummidiLakshman@users.noreply.github.com>
2022-09-10 11:57:38 +0530

    import java.util.Scanner; class Color {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int ch;         ch=sc.nextInt();         //for(ch=1;ch<=12;ch++)         //{         if(ch==4||ch==5||ch==6)         {             System.out.println("Summer");         }         else if(ch==7||ch==8||ch==9||ch==10)         {             System.out.println("Rainy");         }         else if(ch==11||ch==12||ch==1)         {             System.out.println("Winter");         }         else if(ch==2||ch==3)         {             System.out.println("Spring");         }         else         {             System.out.println("-1");         }         //}     sc.close();     } }

---
+++ Display_Season.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+class Color
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int ch;
+        ch=sc.nextInt();
+        //for(ch=1;ch<=12;ch++)
+        //{
+        if(ch==4||ch==5||ch==6)
+        {
+            System.out.println("Summer");
+        }
+        else if(ch==7||ch==8||ch==9||ch==10)
+        {
+            System.out.println("Rainy");
+        }
+        else if(ch==11||ch==12||ch==1)
+        {
+            System.out.println("Winter");
+        }
+        else if(ch==2||ch==3)
+        {
+            System.out.println("Spring");
+        }
+        else
+        {
+            System.out.println("-1");
+        }
+        //}
+    sc.close();
+    }
+}
