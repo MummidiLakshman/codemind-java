6b74599f99981f1685ee3d9e4b862da4e849fedd
MummidiLakshman <96734843+MummidiLakshman@users.noreply.github.com>
2022-09-25 10:04:55 +0530

    import java.util.Scanner; class code {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int i,j,a,c=0,t=0,p=0,d=0,n,k;         a=sc.nextInt();         int[] x=new int[a];         int[] arr=new int[100];         for(i=0;i<a;i++)         {             x[i]=sc.nextInt();         }         n=sc.nextInt();         k=sc.nextInt();         for(i=0;i<a;i++)         {             if(x[i]>=n && x[i]<=k)             {                 c+=x[i];             }             else             {                 t+=x[i];             }         }         System.out.print(t);     } }

---
+++ sum_of_elements_not_between_A_and_B.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+class code
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int i,j,a,c=0,t=0,p=0,d=0,n,k;
+        a=sc.nextInt();
+        int[] x=new int[a];
+        int[] arr=new int[100];
+        for(i=0;i<a;i++)
+        {
+            x[i]=sc.nextInt();
+        }
+        n=sc.nextInt();
+        k=sc.nextInt();
+        for(i=0;i<a;i++)
+        {
+            if(x[i]>=n && x[i]<=k)
+            {
+                c+=x[i];
+            }
+            else
+            {
+                t+=x[i];
+            }
+        }
+        System.out.print(t);
+    }
+}
