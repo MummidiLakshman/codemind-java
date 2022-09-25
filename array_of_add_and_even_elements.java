b3f0c9e7032cccf21638cbc5ec0aaeb17fa3ac11
MummidiLakshman <96734843+MummidiLakshman@users.noreply.github.com>
2022-09-25 10:13:42 +0530

    import java.util.Scanner; class code {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int i,j,a,c=0,t=0,p=0,d=0,n,k;         a=sc.nextInt();         int[] x=new int[a];         int[] arr=new int[100];         for(i=0;i<a;i++)         {             x[i]=sc.nextInt();         }         for(i=0;i<a;i++)         {             if(x[i]%2!=0)             {                 arr[p++]=x[i];             }         }         for(i=0;i<a;i++)         {             if(x[i]%2==0)             {                 arr[p++]=x[i];             }         }         for(i=0;i<p;i++)             System.out.print(arr[i]+" ");     } }

---
+++ array_of_add_and_even_elements.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+class code
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int i,j,a,c=0,t=0,p=0,d=0,n,k;
+        a=sc.nextInt();
+        int[] x=new int[a];
+        int[] arr=new int[100];
+        for(i=0;i<a;i++)
+        {
+            x[i]=sc.nextInt();
+        }
+        for(i=0;i<a;i++)
+        {
+            if(x[i]%2!=0)
+            {
+                arr[p++]=x[i];
+            }
+        }
+        for(i=0;i<a;i++)
+        {
+            if(x[i]%2==0)
+            {
+                arr[p++]=x[i];
+            }
+        }
+        for(i=0;i<p;i++)
+            System.out.print(arr[i]+" ");
+    }
+}
