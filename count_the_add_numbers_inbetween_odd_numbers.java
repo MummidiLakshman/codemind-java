cef0bec8c3145cefbb55e0e5a6b1b050a78e9550
MummidiLakshman <96734843+MummidiLakshman@users.noreply.github.com>
2022-09-26 19:32:29 +0530

    import java.util.*; class Code {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,i,c=0;         n=sc.nextInt();         int x[]=new int[n];         for(i=0;i<n;i++)             x[i]=sc.nextInt();         for(i=1;i<n-1;i++)         {             if(x[i-1]%2!=0 && x[i+1]%2!=0)             {                 if(x[i]%2!=0)                     c+=1;             }         }         System.out.print(c);     } }

---
+++ count_the_add_numbers_inbetween_odd_numbers.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class Code
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,i,c=0;
+        n=sc.nextInt();
+        int x[]=new int[n];
+        for(i=0;i<n;i++)
+            x[i]=sc.nextInt();
+        for(i=1;i<n-1;i++)
+        {
+            if(x[i-1]%2!=0 && x[i+1]%2!=0)
+            {
+                if(x[i]%2!=0)
+                    c+=1;
+            }
+        }
+        System.out.print(c);
+    }
+}
