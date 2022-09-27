be97156bdc39837a644a9651192e6f2f400fdb3f
MummidiLakshman <96734843+MummidiLakshman@users.noreply.github.com>
2022-09-27 19:48:51 +0530

    import java.util.*; class Code {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,i,j,s;         n=sc.nextInt();         i=sc.nextInt();         j=sc.nextInt();         s=(n*i*j)/100;         System.out.print(s);     } }

---
+++ SIMPLE_INTEREST.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Code
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,i,j,s;
+        n=sc.nextInt();
+        i=sc.nextInt();
+        j=sc.nextInt();
+        s=(n*i*j)/100;
+        System.out.print(s);
+    }
+}
