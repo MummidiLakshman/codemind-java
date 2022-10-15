2fd4176b13bbe1a8b4e4171286fb2371e413decb
MummidiLakshman <96734843+MummidiLakshman@users.noreply.github.com>
2022-10-15 13:51:26 +0530

    import java.util.*; class code {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int x[]=new int[n];         for(int i=0;i<n;i++)         {             x[i]=sc.nextInt();         }         Arrays.sort(x);         for(int i=0;i<n;i++)         System.out.print(x[i]+" ");     } }

---
+++ Sort_an_Array_of_0s_and_1s.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class code
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int x[]=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            x[i]=sc.nextInt();
+        }
+        Arrays.sort(x);
+        for(int i=0;i<n;i++)
+        System.out.print(x[i]+" ");
+    }
+}
